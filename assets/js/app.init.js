5710993799ecd2fa6995f38abf48a993664128a9
susephere <susepsupriatna@gmail.com>
2023-05-25 14:18:59 +0700

    update

---
+++ assets/js/app.init.js
@@ -0,0 +1,8 @@
+$(function () {
+  "use strict";
+  $("#main-wrapper").AdminSettings({
+    // IS HERE //
+    ThemeBg: "aqua_theme", //
+    SidebarType: "full", // You can change it full / mini-sidebar
+  });
+});
